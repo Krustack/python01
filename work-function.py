95a9ad95c7130018380fec64926e5ff82513ccaa
ชืwisanu <pretzx@gmail.com>
2022-07-25 11:09:52 +0700

    work-function

---
+++ work-function.py
@@ -0,0 +1,11 @@
+#สร้างไฟล์work-function.py โดยจะมีฟังก์ชั่นดังนี้
+def square(width,height):
+  #คำสั่งหาพื้นที่สี่เหลี่ยม
+  area = width*height
+  print("พื้นที่สี่เหลี่ยม:",area)
+def triangle(base,height):
+     #คำสั่งหาพื้นที่สามเหลี่ยม
+def circle(radius):
+  #คำสั่งหาพื้นที่วงกลม
+
+#ชื่อ เลขที่ ชั้น
